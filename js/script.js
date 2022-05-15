9d025aad4912e5fc99bab9a8ec252658221b8f9a
andrey <domaratskiy1990@gmail.com>
2022-05-15 17:45:26 +0300

    portfolio v1.0

--- js/script.js
+++ js/script.js
@@ -1,21 +1,46 @@
-let humburger = document.querySelector('.nav_cont');
+let humburger = document.querySelector('.burger');
 let Mymenu = document.querySelector('.menu');
-
 
 Mymenu.style.display = 'none';
 humburger.onclick = () =>  Mymenu.style.display == 'none' ? Mymenu.style.display = 'block' : Mymenu.style.display = 'none';
 
 
+function hideMenuScript(){
+   Mymenu.style.display = 'none'; 
+};
+ 
 
-$('.logo_cont').fadeOut().fadeIn(2000);
+let nameGulp = document.querySelector('.webSkills a[data-name~="name-gulp"]');
+let nameJs = document.querySelector('.webSkills a[data-name~="name-js"]');
+let nameHtml = document.querySelector('.webSkills a[data-name~="name-html"]');
+let nameCss = document.querySelector('.webSkills a[data-name~="name-css"]');
+let nameSass = document.querySelector('.webSkills a[data-name~="name-sass"]');
 
-let menumap = document.querySelector('.thoGiss');
-let menuLoc = document.querySelector('.locMap');
+
+setTimeout( () => {
+	addStyle(nameHtml, `translateY(0)`);
+},500);
+setTimeout( () => {
+	addStyle(nameJs, `translateY(0)`);
+	addStyle(nameCss, `translateY(0)`);
+},1000);
+setTimeout( () => {
+	addStyle(nameGulp, `translateY(0)`);
+	addStyle(nameSass, `translateY(0)`);
+},1500);
 
 
-menumap.style.display = 'none';
-menuLoc.onclick = () =>  menumap.style.display == 'none' ? menumap.style.display = 'block' : menumap.style.display = 'none';
+function addStyle(name, value){
+	name.style.transform = value; 
+}
 
-let emailJs = document.querySelector("#getMail");
+let section2_text = document.querySelector('.myText__text');
+
+
+setTimeout( () => {
+	section2_text.style.opacity = 1;	
+},1500);
+
+
 
 
